ea03d1eb2955d1630ad322cb8e4fc83b854a1a15
Nischal Mainali <nischalmainali21@gmail.com>
2024-05-03 12:34:20 +0545

    Update UI of adding new todos - remove the input field for title and description, replace with a new to-do button - add a filter button for creating multiple views of the todos, the to-do and completed button will be replaced - change alignment of the header and button from center to left for the redesign - the new to-do and filter button will open a modal view for creating a new to-do and filter view

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import { Button } from "@/components/ui/button";
-
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { ListFilter } from "lucide-react";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [todo, setTodo] = useState({ todoTitle: "", todoContent: "" });
@@ -32,30 +34,54 @@ function App() {
 
   return (
     <>
-      <div className="flex  items-center flex-col gap-8">
-        <h1 className="text-3xl">MY TODOs</h1>
-        <div className="shadow-md p-4 flex flex-col gap-8">
-          <TodoForm todo={todo} handleChange={handleChange} addTodo={addTodo} />
-          <div className="h-1 text-red-400">
-            {titleError && "Title cannot be empty"}
+      <div className="flex items-center flex-col pt-8 ">
+        <div className="flex flex-col gap-6 min-w-[70%]">
+          <h1 className="text-3xl">To-Do</h1>
+          <Separator />
+          <div className=" flex flex-col gap-8">
+            {/* <TodoForm
+              todo={todo}
+              handleChange={handleChange}
+              addTodo={addTodo}
+            /> */}
+
+            <div className="flex gap-2 items-center">
+              <Button className="flex gap-1 items-center">
+                <span>
+                  <Plus />
+                </span>
+                <span>New To-Do</span>
+              </Button>
+              <Button className="flex gap-1 items-center">
+                <span>
+                  <ListFilter />
+                </span>
+                <span>Filter</span>
+              </Button>
+            </div>
+            {/* <div className="h-1 text-red-400">
+              {titleError && "Title cannot be empty"}
+            </div> */}
+            <div className="flex items-center gap-3">
+              <Button onClick={() => setStatusView("active")}>To Do</Button>
+              <Button onClick={() => setStatusView("complete")}>
+                Completed
+              </Button>
+            </div>
+            {statusView === "complete" ? (
+              <TodoList
+                todoList={todoList.filter((item) => item.status === "complete")}
+                setTodoList={setTodoList}
+                statusView={statusView}
+              />
+            ) : (
+              <TodoList
+                todoList={todoList.filter((item) => item.status === "active")}
+                setTodoList={setTodoList}
+                statusView={statusView}
+              />
+            )}
           </div>
-          <div className="flex items-center gap-3">
-            <Button onClick={() => setStatusView("active")}>To Do</Button>
-            <Button onClick={() => setStatusView("complete")}>Completed</Button>
-          </div>
-          {statusView === "complete" ? (
-            <TodoList
-              todoList={todoList.filter((item) => item.status === "complete")}
-              setTodoList={setTodoList}
-              statusView={statusView}
-            />
-          ) : (
-            <TodoList
-              todoList={todoList.filter((item) => item.status === "active")}
-              setTodoList={setTodoList}
-              statusView={statusView}
-            />
-          )}
         </div>
       </div>
     </>
