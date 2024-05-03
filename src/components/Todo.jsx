6da82064b0c2abaaf26902353f241a12ba8f618f
Nischal Mainali <nischalmainali21@gmail.com>
2024-05-03 15:59:05 +0545

    Replace complete btn with checkbox for todo item - move the complete icon-btn to checkbox, to create a clear difference between completed and active - completed todo needs to be styled

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,3 +1,7 @@
+import EditableText from "./EditableText";
+
+import { cn } from "@/lib/utils";
+
 import {
   Card,
   CardDescription,
@@ -6,8 +10,9 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Trash2, Check } from "lucide-react";
-import EditableText from "./EditableText";
+import { Checkbox } from "@/components/ui/checkbox";
+
+import { Trash2 } from "lucide-react";
 
 const MonthNames = [
   "January",
@@ -33,6 +38,7 @@ const Todo = ({
   handleCompleteTask,
   editTodo,
   statusView,
+  status,
 }) => {
   const dateCreated = new Date(creationTime);
   const day = dateCreated.getDate();
@@ -44,33 +50,47 @@ const Todo = ({
     // editTodo(id, e.target.textContent);
     editTodo(id, e.target.value);
   }
+
   return (
     <Card className="">
       <div className="flex justify-between items-center">
-        <CardHeader>
-          <CardTitle
-            // contentEditable="true"
-            // onInput={(e) => handleChange(e, id)}
-            className="p-2"
-          >
-            <EditableText text={title}>
-              <Input
-                type="text"
-                name="todo"
-                value={title}
-                onChange={(e) => handleChange(e, id)}
-                className="h-8 focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0"
+        <div className="flex items-baseline">
+          <div className="p-3">
+            {/* show checkbox ticked for complete status */}
+            {statusView === "complete" ? (
+              <div></div>
+            ) : (
+              <Checkbox
+                className="w-5 h-5"
+                onCheckedChange={() => handleCompleteTask(id)}
               />
-            </EditableText>
-            {/* {title} */}
-          </CardTitle>
-          <CardDescription>
-            <span className="flex gap-1 items-center text-center text-xs">
-              <span className=" opacity-70 ">{actualDate}</span>
-              <span>{content && content}</span>
-            </span>
-          </CardDescription>
-        </CardHeader>
+            )}
+          </div>
+          <CardHeader className="p-3">
+            <CardTitle
+              // contentEditable="true"
+              // onInput={(e) => handleChange(e, id)}
+              className="p-2 pl-0"
+            >
+              <EditableText text={title}>
+                <Input
+                  type="text"
+                  name="todo"
+                  value={title}
+                  onChange={(e) => handleChange(e, id)}
+                  className="h-8 focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0"
+                />
+              </EditableText>
+              {/* {title} */}
+            </CardTitle>
+            <CardDescription>
+              <span className="flex gap-1 items-center text-center text-xs">
+                <span className=" opacity-70 ">{actualDate}</span>
+                <span>{content && content}</span>
+              </span>
+            </CardDescription>
+          </CardHeader>
+        </div>
         <div className="flex items-center gap-3 pr-6">
           <Button
             variant="outline"
@@ -80,17 +100,6 @@ const Todo = ({
             <Trash2 className="w-4 h-4" />
           </Button>
           {/* todo:conditional render this button on the basis of viewstatus */}
-          {statusView === "complete" ? (
-            <div></div>
-          ) : (
-            <Button
-              variant="outline"
-              size="icon"
-              onClick={() => handleCompleteTask(id)}
-            >
-              <Check className="w-4 h-4" />
-            </Button>
-          )}
         </div>
       </div>
     </Card>
